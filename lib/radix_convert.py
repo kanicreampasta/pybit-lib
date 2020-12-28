fd35226e58e8328a0660ef2c0a0faa35cc9139a9
das08 <takeda5955@gmail.com>
2020-12-28 21:12:37 +0900

    dec -> decの基数変換(正のときのみ)

--- lib/radix_convert.py
+++ lib/radix_convert.py
@@ -2,4 +2,20 @@ class RadixConvert:
 
     @staticmethod
     def dec_to_bin(value: int) -> str:
+        """
+        Convert decimal number to binary.
+        TODO: allow negative decimal
+        :param value: (int) Decimal number.
+        :return: (str) Binary number
+        """
         return bin(value)
+
+    @staticmethod
+    def dec_to_hex(value: int) -> str:
+        """
+        Convert decimal number to hexadecimal number.
+        TODO: allow negative decimal
+        :param value: (int) Decimal number.
+        :return: (str) Hexadecimal number
+        """
+        return hex(value)
--- tests/test_radix_converter.py
+++ tests/test_radix_converter.py
@@ -6,3 +6,9 @@ def test_dec_to_bin():
     assert RadixConvert.dec_to_bin(0) == '0b0'
     assert RadixConvert.dec_to_bin(10) == '0b1010'
     assert RadixConvert.dec_to_bin(100) == '0b1100100'
+
+
+def test_dec_to_hex():
+    assert RadixConvert.dec_to_hex(0) == '0x0'
+    assert RadixConvert.dec_to_hex(10) == '0xa'
+    assert RadixConvert.dec_to_hex(100) == '0x64'
