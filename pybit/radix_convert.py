de0d784fadf3e4623e40cd839ead40544150bee9
das08 <takeda5955@gmail.com>
2020-12-30 18:14:43 +0900

    ２進数の桁数関連

--- pybit/radix_convert.py
+++ pybit/radix_convert.py
@@ -1,14 +1,20 @@
 class RadixConvert:
 
     @staticmethod
-    def dec_to_bin(value: int) -> str:
+    def dec_to_bin(value: int, digits: int = 0) -> str:
         """
         Convert decimal number to binary.
         TODO: allow negative decimal
         :param value: (int) Decimal number.
+        :param digits: (int) Number of digits.
         :return: (str) Binary number
         """
-        return bin(value)
+        binary = ''
+        if value >= 0:
+            binary = '{0:#0{1}b}'.format(value, digits + 2)
+        else:
+            binary = '{:#0{}b}'.format(value, digits)
+        return binary
 
     @staticmethod
     def dec_to_hex(value: int) -> str:
--- tests/test_radix_converter.py
+++ tests/test_radix_converter.py
@@ -5,6 +5,7 @@ from pybit.radix_convert import RadixConvert
 def test_dec_to_bin():
     assert RadixConvert.dec_to_bin(0) == '0b0'
     assert RadixConvert.dec_to_bin(10) == '0b1010'
+    assert RadixConvert.dec_to_bin(10,5) == '0b01010'
     assert RadixConvert.dec_to_bin(100) == '0b1100100'
 
 
