884bf412c9ee630631212fb1e9f80c47aadf78a4
das08 <takeda5955@gmail.com>
2021-01-01 21:08:47 +0900

    今日はここまで！

--- pybit/multiplication.py
+++ pybit/multiplication.py
@@ -1,3 +1,6 @@
+from pybit.bits import Bits
+
+
 class Multiplication:
 
     @staticmethod
@@ -5,8 +8,18 @@ class Multiplication:
         pass
 
     @staticmethod
-    def booth_secondary():
-        pass
+    def booth_secondary(A: Bits, B: Bits, t: str = 'bin'):
+        size = [len(A), len(B)]
+        if size[0] != size[1]:
+            raise TypeError('A and B must be same length')
+        if size[0] != 6:
+            raise TypeError('Only supports 6 bits now')
+
+        # 符号拡張
+        A = Bits([A[0] for _ in range(6)] + [A[i] for i in range(6)])
+        B = Bits([B[0] for _ in range(6)] + [B[i] for i in range(6)])
+
+
 
     @staticmethod
     def booth_tertiary():
