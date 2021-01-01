9834b87351cb89a99231300f04d3282cd97e4698
das08 <takeda5955@gmail.com>
2021-01-01 20:30:25 +0900

    multiplication by booth and other method

---
+++ pybit/multiplication.py
@@ -0,0 +1,21 @@
+class Multiplication:
+
+    @staticmethod
+    def booth_primary():
+        pass
+
+    @staticmethod
+    def booth_secondary():
+        pass
+
+    @staticmethod
+    def booth_tertiary():
+        pass
+
+    @staticmethod
+    def partial_product():
+        pass
+
+    @staticmethod
+    def wallace_tree():
+        pass
---
+++ tests/test_multiplication.py
@@ -0,0 +1,22 @@
+import pytest
+from pybit.multiplication import Multiplication
+
+
+def test_booth_primary():
+    pass
+
+
+def test_booth_secondary():
+    pass
+
+
+def test_booth_tertiary():
+    pass
+
+
+def test_partial_product():
+    pass
+
+
+def test_wallace_tree():
+    pass
