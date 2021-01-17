64e49177373e7ced47136c88505ac9530bf0ba52
das08 <takeda5955@gmail.com>
2021-01-18 01:51:47 +0900

    テスト追加

--- pybit/multiplication.py
+++ pybit/multiplication.py
@@ -3,7 +3,6 @@ from pybit.bits import Bits
 
 class Multiplication:
 
-
     @staticmethod
     def booth_primary():
         pass
@@ -27,35 +26,34 @@ class Multiplication:
             raise TypeError('Only supports 6 bits now')
 
         # 符号拡張
-        A = A.sign_extend(size=size[0]*2)
-        B = B.sign_extend(size=size[1]*2)
+        A = A.sign_extend(size=size[0] * 2)
+        B = B.sign_extend(size=size[1] * 2)
         B = B << ('l', 1)
 
-        result = []
+        pp = []
 
         for i in range(3):
             bit3 = ''.join(map(str, B[12 - 3 - 2 * i:12 - 2 * i]))
 
             if PPType[bit3] == "0":
-                result.append(Bits.from_dec(0, 12))
+                pp.append(Bits.from_dec(0, 12))
             elif PPType[bit3] == "+1":
-                result.append(A << ('l', 2*i))
+                pp.append(A << ('l', 2 * i))
             elif PPType[bit3] == "+2":
-                result.append(A << ('l', 1) << ('l', 2*i))
+                pp.append(A << ('l', 1) << ('l', 2 * i))
             elif PPType[bit3] == "-1":
                 invA = A.__invert__()
-                result.append(invA << ('l', 2*i))
+                pp.append(invA << ('l', 2 * i))
             elif PPType[bit3] == "-2":
                 invA = A.__invert__()
-                result.append(invA << ('l', 1) << ('l', 2*i))
+                pp.append(invA << ('l', 1) << ('l', 2 * i))
             else:
                 raise TypeError("unintended bit3")
-        for i in result:
+        for i in pp:
             print(i[0:])
+        ans = Bits([0])
 
-
-
-        return [A, B]
+        return pp.append(ans)
 
     @staticmethod
     def booth_tertiary():
@@ -68,7 +66,6 @@ class Multiplication:
     @staticmethod
     def wallace_tree():
         pass
-
 
 
 # print(Multiplication.booth_secondary(Bits.from_dec(10, 6), Bits.from_dec(-1, 6)))
--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -1,4 +1,5 @@
 import pytest
+from pybit.bits import Bits
 from pybit.multiplication import Multiplication
 
 
@@ -7,7 +8,15 @@ def test_booth_primary():
 
 
 def test_booth_secondary():
-    pass
+    A1 = Bits.from_dec(0b010111, 6)
+    B1 = Bits.from_dec(0b001011, 6)
+    assert Multiplication.booth_secondary(A1, B1) == [
+        Bits([1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1]),
+        Bits([1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0]),
+        Bits([0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0]),
+        Bits([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1])
+    ]
+
 
 
 def test_booth_tertiary():
