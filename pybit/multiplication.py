fdd4dd4d649a9c4b80a66b3af393a6f2f65e4073
das08 <takeda5955@gmail.com>
2021-01-18 01:25:06 +0900

    あとは実装するだけ

--- pybit/multiplication.py
+++ pybit/multiplication.py
@@ -3,12 +3,23 @@ from pybit.bits import Bits
 
 class Multiplication:
 
+
     @staticmethod
     def booth_primary():
         pass
 
     @staticmethod
     def booth_secondary(A: Bits, B: Bits, t: str = 'bin'):
+        PPType = {
+            '000': "0",
+            '001': "+1",
+            '010': "+1",
+            '011': "+2",
+            '100': "-2",
+            '101': "-1",
+            '110': "-1",
+            '111': "0"
+        }
         size = [len(A), len(B)]
         if size[0] != size[1]:
             raise TypeError('A and B must be same length')
@@ -16,10 +27,29 @@ class Multiplication:
             raise TypeError('Only supports 6 bits now')
 
         # 符号拡張
-        A = Bits([A[0] for _ in range(6)] + [A[i] for i in range(6)])
-        B = Bits([B[0] for _ in range(6)] + [B[i] for i in range(6)])
+        A = A.sign_extend(size=size[0]*2)
+        B = B.sign_extend(size=size[1]*2)
+        B = B << ('l', 1)
+
+        for i in range(3):
+            bit3 = ''.join(map(str, B[12 - 3 - 2 * i:12 - 2 * i]))
+
+            if PPType[bit3] == "0":
+                pass
+            elif PPType[bit3] == "+1":
+                pass
+            elif PPType[bit3] == "+2":
+                pass
+            elif PPType[bit3] == "-1":
+                pass
+            elif PPType[bit3] == "-2":
+                pass
+            else:
+                raise TypeError("unintended bit3")
 
 
+
+        return [A, B]
 
     @staticmethod
     def booth_tertiary():
@@ -32,3 +62,4 @@ class Multiplication:
     @staticmethod
     def wallace_tree():
         pass
+
