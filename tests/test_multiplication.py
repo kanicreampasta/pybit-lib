13888b3edcec5b07fd9c3d769a024e4719356d75
das08 <takeda5955@gmail.com>
2021-01-18 16:38:39 +0900

    テスト追加

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -16,6 +16,14 @@ def test_booth_secondary():
         Bits([0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0]),
         Bits([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1])
     ]
+    A2 = Bits.from_dec(0b101111, 6)
+    B2 = Bits.from_dec(0b011010, 6)
+    assert Multiplication.booth_secondary(A2, B2) == [
+        Bits([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0]),
+        Bits([0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]),
+        Bits([1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0]),
+        Bits([1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0])
+    ]
 
 
 
